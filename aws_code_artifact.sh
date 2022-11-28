fc5684f7bc27c20ab9fe5fe2a4bcfd0b424f0609
Damian Stanger <damian.stanger@lexisnexisrisk.com>
2022-11-28 22:55:58 +0000

    docs and small fixes

--- aws_code_artifact.sh
+++ aws_code_artifact.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 rm -f aws_code_artifact.txt
-rm -f aws_code_artifact_eg1.txt
-rm -f aws_code_artifact_eg2.txt
-rm -f wrong_latest.txt
+rm -f aws_code_artifact_1.txt
+rm -f aws_code_artifact_2.txt
+rm -f package_versions.txt
+rm -f this_versions.txt
 rm -f all_versions.txt
 
 domain=$1
@@ -11,23 +12,23 @@ domain_owner=$2
 
 aws codeartifact list-packages --domain $domain --domain-owner $domain_owner --repository npm-private --profile saml >> aws_code_artifact.txt
 
-grep -A 1 -Pe '"namespace": "eg",' aws_code_artifact.txt >> aws_code_artifact_eg1.txt
-sed -i -E '/.*("namespace"|--).*/d' aws_code_artifact_eg1.txt
-sed -i -E 's/.*": "//' aws_code_artifact_eg1.txt
-sed -i -E 's/"//' aws_code_artifact_eg1.txt
-# sed -i -E 's|^|@eg/|' aws_code_artifact_eg1.txt
+grep -A 1 -Pe "\"namespace\": \"$domain\"," aws_code_artifact.txt >> aws_code_artifact_1.txt
+sed -i -E '/.*("namespace"|--).*/d' aws_code_artifact_1.txt
+sed -i -E 's/.*": "//' aws_code_artifact_1.txt
+sed -i -E 's/"//' aws_code_artifact_1.txt
 
-grep -Pe '"(eg|@eg)[^"]+"' aws_code_artifact.txt >> aws_code_artifact_eg2.txt
-sed -i -E 's/.*": "//' aws_code_artifact_eg2.txt
-sed -i -E 's/"//' aws_code_artifact_eg2.txt
+grep -Pe "\"($domain|@$domain)[^\"]+\"" aws_code_artifact.txt >> aws_code_artifact_2.txt
+sed -i -E 's/.*": "//' aws_code_artifact_2.txt
+sed -i -E 's/"//' aws_code_artifact_2.txt
 
+exit 0
 
-cat aws_code_artifact_eg1.txt | while read line
+cat aws_code_artifact_1.txt | while read line
 do
-  echo "@eg/$line"
+  echo "@$domain/$line"
   rm -f package_versions.txt
   rm -f this_versions.txt
-  aws codeartifact list-package-versions --package "$line" --namespace eg --format npm --domain $domain --domain-owner $domain_owner --repository npm-private --profile saml >> package_versions.txt
+  aws codeartifact list-package-versions --package "$line" --namespace $domain --format npm --domain $domain --domain-owner $domain_owner --repository npm-private --profile saml >> package_versions.txt
   latest1=$(grep -oPe '"defaultDisplayVersion": "([0-9\.]+)",' package_versions.txt)
   latest1=$(echo "$latest1" | grep -oPe "[0-9\.]+")
 
@@ -36,17 +37,17 @@ do
   latest2=$(cat this_versions.txt | sort -V | tail -n 1)
 
   if [ "$latest1" = "$latest2" ]; then
-    echo "@eg/$line:$latest1 == $latest2" >> all_versions.txt
+    echo "@$domain/$line:$latest1 == $latest2" >> all_versions.txt
   else
-    echo "@eg/$line:$latest1 != $latest2   -------------   :-("
-    echo "@eg/$line:$latest1 != $latest2   -------------   :-(" >> all_versions.txt
+    echo "@$domain/$line:$latest1 != $latest2   -------------   :-("
+    echo "@$domain/$line:$latest1 != $latest2   -------------   :-(" >> all_versions.txt
   fi
 
-  cat this_versions.txt | xargs echo "@eg/$line" >> all_versions.txt
+  cat this_versions.txt | xargs echo "@$domain/$line" >> all_versions.txt
 done
 
 
-cat aws_code_artifact_eg2.txt | while read line
+cat aws_code_artifact_2.txt | while read line
 do
   echo $line
   rm -f package_versions.txt
